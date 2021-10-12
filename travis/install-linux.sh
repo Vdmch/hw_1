e03d72bc49a61d0830f30e85ae853ab4a9fe3244
Vadim <vdmchu@gmail.com>
2021-10-12 17:19:24 +0000

    initial commit

---
+++ travis/install-linux.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+
+sudo apt-get install build-essential
+sudo apt-get install clang-format
+sudo apt-get install valgrind
+sudo apt-get install cppcheck
+sudo pip install gcovr
