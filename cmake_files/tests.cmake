f8225b78dcab44cf13a81e31ffd3f99bd848a109
Vadim <vdmchu@gmail.com>
2021-10-13 19:52:27 +0000

    tst_commit

--- cmake_files/tests.cmake
+++ cmake_files/tests.cmake
@@ -3,63 +3,31 @@
 ################################
 
 set(test_files test_func.cpp)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread  -static-libtsan")
-
-# Добавление файлов googletest
-set(GOOGLETEST_ROOT googletest/googletest CACHE STRING "Google Test source root")
-include_directories(SYSTEM
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
-    )
-
-set(GOOGLETEST_SOURCES
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
-    )
-
-foreach(_source ${GOOGLETEST_SOURCES})
-    set_source_files_properties(${_source} PROPERTIES GENERATED 1)
-endforeach()
 
 enable_testing()
+find_package(GTest REQUIRED)
+find_package(Threads REQUIRED)
 
 
-
-add_library(test_lib  ${lib_files})
-add_library(googletest ${GOOGLETEST_SOURCES})
-
-
+include_directories(${GTEST_INCLUDE_DIRS})
+add_library(test_lib STATIC ${lib_files})
 
 # Добавление тестов с санитайзерами undefined, leak и address
 add_executable(runUnitTests ${test_files})
-
-set(TEST0_CXX_COMPILER_FLAGS, "${CMAKE_CXX_FLAGS} -fsanitize=undefined -fsanitize=leak -fsanitize=address -O0 -g")
-set(TEST0_C_COMPILER_FLAGS, "${CMAKE_C_FLAGS} -fsanitize=undefined -fsanitize=leak -fsanitize=address -O0 -g")
-
-target_compile_options(runUnitTests PRIVATE 
-    $<$<COMPILE_LANGUAGE:CXX>: ${TEST0_CXX_COMPILER_FLAGS}> 
-    $<$<COMPILE_LANGUAGE:C>: ${TEST0_C_COMPILER_FLAGS}>
-)
-
-target_link_libraries(runUnitTests googletest)
-target_link_libraries(runUnitTests test_lib)
+target_link_libraries(runUnitTests ${GTEST_LIBRARIES} test_lib pthread gcov)
+target_compile_options(runUnitTests PRIVATE -O0 -g)
+target_link_options(runUnitTests PRIVATE -static-libasan -fsanitize=undefined -fsanitize=leak -fsanitize=address)
 add_test(runUnitTests runUnitTests)
-
-
-
 
 # Добавление тестов с санитайзером thread
 add_executable(runUnitTests_thread ${test_files})
-
-set(TEST1_CXX_COMPILER_FLAGS, "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage -fsanitize=thread -O0 -g")
-set(TEST1_C_COMPILER_FLAGS, "${CMAKE_C_FLAGS} -fprofile-arcs -ftest-coverage -fsanitize=thread -O0 -g")
-
-target_compile_options(runUnitTests_thread PRIVATE 
-    $<$<COMPILE_LANGUAGE:CXX>: ${TEST1_CXX_COMPILER_FLAGS}> 
-    $<$<COMPILE_LANGUAGE:C>: ${TEST1_C_COMPILER_FLAGS}>
-    )
-
-target_link_libraries(runUnitTests_thread test_lib)
-target_link_libraries(runUnitTests_thread googletest)
+target_link_libraries(runUnitTests_thread ${GTEST_LIBRARIES} test_lib pthread gcov)
+target_compile_options(runUnitTests_thread PRIVATE -fprofile-arcs -O0 -g)
+target_link_options(runUnitTests_thread PRIVATE -static-libasan -fsanitize=thread)
 add_test(runUnitTests_thread runUnitTests_thread)
 
+# Добавление тестов с санитайзером valgrind
+add_executable(runUnitTests_valgrind ${test_files})
+target_link_libraries(runUnitTests_valgrind ${GTEST_LIBRARIES} test_lib pthread)
+target_compile_options(runUnitTests_valgrind PRIVATE -O0 -g)
+add_test(runUnitTests_valgrind runUnitTests_valgrind)
