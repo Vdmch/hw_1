244ff1b92ff214a88430103a8270a68f62137b4b
Vadim <vdmchu@gmail.com>
2021-10-13 19:56:06 +0000

    tst_commit

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,6 +1,7 @@
 sudo apt-get update -y
 
 sudo apt-get install build-essential
+sudo apt-get install libgtest-dev
 sudo apt-get install clang-tools
 sudo apt-get install valgrind
 sudo apt-get install cppcheck
