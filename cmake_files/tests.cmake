c9866fd91de64f653a4222bcd397545f04b8f411
Vadim <vdmchu@gmail.com>
2021-10-13 20:24:07 +0000

    tst_commit

--- cmake_files/tests.cmake
+++ cmake_files/tests.cmake
@@ -4,30 +4,32 @@
 
 set(test_files test_func.cpp)
 
-enable_testing()
 find_package(GTest REQUIRED)
-find_package(Threads REQUIRED)
+enable_testing()
 
-
-include_directories(${GTEST_INCLUDE_DIRS})
 add_library(test_lib STATIC ${lib_files})
 
 # Добавление тестов с санитайзерами undefined, leak и address
 add_executable(runUnitTests ${test_files})
-target_link_libraries(runUnitTests ${GTEST_LIBRARIES} test_lib pthread gcov)
-target_compile_options(runUnitTests PRIVATE -O0 -g)
+target_link_libraries(runUnitTests GTest::Main test_lib pthread)
+target_compile_options(runUnitTests PRIVATE -O0 -g -static-libasan -fsanitize=undefined -fsanitize=leak -fsanitize=address)
 target_link_options(runUnitTests PRIVATE -static-libasan -fsanitize=undefined -fsanitize=leak -fsanitize=address)
 add_test(runUnitTests runUnitTests)
 
 # Добавление тестов с санитайзером thread
 add_executable(runUnitTests_thread ${test_files})
-target_link_libraries(runUnitTests_thread ${GTEST_LIBRARIES} test_lib pthread gcov)
-target_compile_options(runUnitTests_thread PRIVATE -fprofile-arcs -O0 -g)
+target_link_libraries(runUnitTests_thread GTest::Main test_lib pthread)
+target_compile_options(runUnitTests_thread PRIVATE -O0 -g -static-libasan -fsanitize=thread)
 target_link_options(runUnitTests_thread PRIVATE -static-libasan -fsanitize=thread)
 add_test(runUnitTests_thread runUnitTests_thread)
 
 # Добавление тестов с санитайзером valgrind
 add_executable(runUnitTests_valgrind ${test_files})
-target_link_libraries(runUnitTests_valgrind ${GTEST_LIBRARIES} test_lib pthread)
-target_compile_options(runUnitTests_valgrind PRIVATE -O0 -g)
+
+target_link_libraries(runUnitTests_valgrind GTest::Main test_lib pthread gcov)
+target_link_libraries(test_lib GTest::Main pthread gcov)
+
+target_compile_options(runUnitTests_valgrind PRIVATE -O0 -g --coverage)
+target_compile_options(test_lib PRIVATE -O0 -g --coverage)
+
 add_test(runUnitTests_valgrind runUnitTests_valgrind)
